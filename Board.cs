c614f4faffe1c8b5f9cc72e1e3c4435eb03fb558
Thomas Nordvig Hermansen <thomasnhprivat@gmail.com>
2018-11-18 23:01:24 +0100

    Coins Are Now Able To Fall, Done

--- Board.cs
+++ Board.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// TODO : Make Coin Fall Down The Rack And Reset Game
+
 namespace connect4
 {
 	class Board : Connect4
@@ -36,32 +38,117 @@ namespace connect4
 			int x = location.X / 100;
 			int y = location.Y / 100;
 
+			// Old Placement Detection
+			#region Placement commented out
+			//int x = 0;
+			//int y = 0;
+
+			//// Row value
+			//if (location.X < 100)
+			//{
+			//	x = 0;
+			//}
+			//else if (location.X > 100 && location.X < 200)
+			//{
+			//	x = 1;
+			//}
+			//else if (location.X > 200 && location.X < 300)
+			//{
+			//	x = 2;
+			//}
+			//else if (location.X > 300 && location.X < 400)
+			//{
+			//	x = 3;
+			//}
+			//else if (location.X > 400 && location.X < 500)
+			//{
+			//	x = 4;
+			//}
+			//else if (location.X > 500 && location.X < 600)
+			//{
+			//	x = 5;
+			//}
+			//else if (location.X > 600 && location.X < 700)
+			//{
+			//	x = 6;
+			//}
+
+			//// Column Value
+			//if (location.Y < 100)
+			//{
+			//	y = 0;
+			//}
+			//else if (location.Y > 100 && location.Y < 200)
+			//{
+			//	y = 1;
+			//}
+			//else if (location.Y > 200 && location.Y < 300)
+			//{
+			//	y = 2;
+			//}
+			//else if (location.Y > 300 && location.Y < 400)
+			//{
+			//	y = 3;
+			//}
+			//else if (location.Y > 400 && location.Y < 500)
+			//{
+			//	y = 4;
+			//}
+			//else if (location.Y > 500 && location.Y < 600)
+			//{
+			//	y = 5;
+			//}
+			#endregion
+
+			// Turn check, and valid placement check
 			if (BoardData[x, y].GetValue() == White)
 			{
+				bool ColumnIsFilled = false;
+
 				if (turn % 2 == 0)
 				{
-					Gfx.DrawRed(new Point(x, y));
-					BoardData[x, y].SetValue(Red);
-					DetectFourInARow(Red);
+					for (int col = BoardData.GetLength(1) - 1; col >= 0; col--)
+					{
+						if (BoardData[x, col].GetValue() == White)
+						{
+							Gfx.DrawRed(new Point(x, col));
+							BoardData[x, col].SetValue(Red);
+							DetectFourInARow(Red);
+							break;
+						}
+						else if (col == BoardData.GetLength(1))
+						{
+							ColumnIsFilled = true;
+						}
+					}
 				}
 				else
 				{
-					Gfx.DrawYellow(new Point(x, y));
-					BoardData[x, y].SetValue(Yellow);
-					DetectFourInARow(Yellow);
+					for (int col = BoardData.GetLength(1) - 1; col >= 0; col--)
+					{
+						if (BoardData[x, col].GetValue() == White)
+						{
+							Gfx.DrawYellow(new Point(x, col));
+							BoardData[x, col].SetValue(Yellow);
+							DetectFourInARow(Yellow);
+							break;
+						}
+						else if (col == BoardData.GetLength(1))
+						{
+							ColumnIsFilled = true;
+						}
+					}
 				}
+
 				turn++;
 			}
 		}
 
-		public void TheChampionIsPlayer(string playerColor)
-		{
-			MessageBox.Show(playerColor + " Player Wins!");
-		}
+		public void TheChampionIsPlayer(string playerColor) => MessageBox.Show(playerColor + " Player Wins!");
 
 		public void DetectFourInARow(int winnerColor)
 		{
-			// verticalCheck (|)
+			// horizontalCheck (-)
 			for (int row = 0; row < BoardData.GetLength(0) - 3; row++)
 			{
 				for (int col = 0; col < BoardData.GetLength(1); col++)
@@ -73,7 +160,7 @@ namespace connect4
 				}
 			}
 
-			// horizontalCheck (-)
+			// verticalCheck (|)
 			for (int row = 0; row < BoardData.GetLength(0); row++)
 			{
 				for (int col = 0; col < BoardData.GetLength(1) - 3; col++)
